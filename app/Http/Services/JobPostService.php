7b71465adf703a86bc652b0d2025d559991c063e
i-ceu <isaacchimdi@gmail.com>
2025-08-08 10:26:54 +0100

    todo

--- app/Http/Services/JobPostService.php
+++ app/Http/Services/JobPostService.php
@@ -1,2 +1,17 @@
 <?php
 
+
+namespace App\Http\Services;
+
+use App\Http\Requests\JobApplicationRequest;
+use App\Http\Resources\JobApplicationResource;
+use App\Models\JobApplication;
+use App\Models\JobPost;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+class JobPostService 
+{
+    // todo: move core functions from controller to service
+}
